65eb3e9713c8b5b4e2dd082d54148a6b3a37702d
Tuton-aka-Cq <84126396+Tuton-aka-Cq@users.noreply.github.com>
2021-05-13 20:58:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+items = ["<:yashiko_socute:817384253515497492>","<:whoisthis:822198965747187752>","<:soulspark_taki:839227218022367285>"]
 
 
 @bot.event
@@ -16,7 +19,8 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def slot(ctx):
     await ctx.send(':slot_machine:')
-    await ctx.send("<:yashiko_socute:817384253515497492><:whoisthis:822198965747187752><:soulspark_taki:839227218022367285>")
+    #await ctx.send("<:yashiko_socute:817384253515497492><:whoisthis:822198965747187752><:soulspark_taki:839227218022367285>")
+    random.sample(items, 3)
 
 
 bot.run(token)
