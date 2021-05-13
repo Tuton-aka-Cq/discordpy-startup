0e96358aebadb5984c62aa634d05b190917b1312
Tuton-aka-Cq <84126396+Tuton-aka-Cq@users.noreply.github.com>
2021-05-13 19:41:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def slot(ctx):
     await ctx.send(':slot_machine:')
-    await message.channel.send("<:whoisthis:822198965747187752>")
+    await ctx.send("<:whoisthis:822198965747187752>")
 
 
 bot.run(token)
